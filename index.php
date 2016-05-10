c1a2edad1ac28382a64177d2593e9309b06390f2
HoYean <comforest@naver.com>
2016-05-10 18:07:55 +0900

    nav structure

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="utf-8">
 	<title>TEST</title>
+	<link rel="stylesheet" type="text/css" href="nav.css">
 </head>
 <body>
 
 	<?php
-		echo("header");
 		include("menu.html");
+		echo("header");
 		echo("footer");
 
 	?>
