fd9ef2b5b4c775d0dce46dff9ea82b3faa320dc8
HoYean <comforest@naver.com>
2016-05-16 00:58:40 +0900

    login & logout structure

--- login.php
+++ login.php
@@ -7,7 +7,7 @@
 </head>
 <body>
 	<form action="login_action.php"  method="POST" >
-		ID : <input id="input-id" type="text" name="id" required>
+		ID : <input id="input-id" type="text" name="id" required><br>
 		PASSWORD : <input id="input-pw" type="password" name="pass" required><br>
 		<input type="submit" value="로그인">
 		<input type="hidden" name="url" value= <?php echo $_SERVER['HTTP_REFERER'] ?>>
