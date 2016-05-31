17fb7a91b9bf533ccb09fc9a28608c09d64a75b1
HoYean <comforest@naver.com>
2016-05-31 15:26:50 +0900

    Change mysql -> mysqli

--- calendar.php
+++ calendar.php
@@ -1,31 +1,30 @@
 <?php
 
 	header("Content-Type:application/json");
-	$db = mysql_connect('localhost:3306','root','1234');
+	$db = mysqli_connect('localhost:3306','root','1234',"vdk");
 	if (!$db) {
-		die('Could not connect: ' . mysql_error());
+		die('Could not connect: ' . mysqli_error());
 	}
-	mysql_select_db("vdk");
 
 	$date = $_POST["year"]."-".$_POST["month"]."-1";
 	$ldate = $_POST["year"]."-".((int)$_POST["month"]+1)."-1";
 	$query = "SELECT date,text,url FROM calendar WHERE date >= '".$date."' and date < '".$ldate."' ORDER BY date ASC";
-	$result = mysql_query($query);
+	$result = mysqli_query($query);
 	if (!$result) {
-		die('Invalid query: ' . mysql_error());
+		die('Invalid query: ' . mysqli_error());
 	}
 
-	while($field=@mysql_fetch_field ($result)){
+	while($field=mysqli_fetch_field ($result)){
 
 	}
 
 	$arr;
-	while($data = mysql_fetch_row($result)){
+	while($data = mysqli_fetch_row($result)){
  		$arr[] = array ('date' => $data[0], 'text' => $data[1], 'url' => $data[2]);
 	}
 
 
-	mysql_close($db);
+	mysqli_close($db);
 	echo json_encode($arr);
 	
 ?>
