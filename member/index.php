0b95a1ed7a20535cc37573aefa50bcc7ea9544a0
HoYean <comforest@naver.com>
2016-07-26 03:10:03 +0900

    Member List Whole Data

--- member/index.php
+++ member/index.php
@@ -1,4 +1,9 @@
-<?php session_start() ?>
+<?php
+	session_start();
+	if(!isset($_SESSION["user"])){
+		header('Location: $_SERVER["DOCUMENT_ROOT"]');
+	}
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -17,12 +22,28 @@
 		<table>
 			<thead>
 				<tr>
-					<th> 이름 </th>
-					<th> 학과 </th>
-					<th> 학번 </th>
-					<th> 등급 </th>
-					<th> 활동 지역 </th>
-					<th> 활동 여부 </th>
+				<?php
+					if($_SESSION["rank"] > 2){
+						echo "<th>이름</th>";
+						echo "<th>학과</th>";
+						echo "<th>학번</th>";
+						echo "<th>등급</th>";
+						echo "<th>활동 지역</th>";
+						echo "<th>활동 여부</th>";
+					}else{
+						echo "<th>이름</th>";
+						echo "<th>성별</th>";
+						echo "<th>단과대</th>";
+						echo "<th>학과</th>";
+						echo "<th>학번</th>";
+						echo "<th>전화 번호</th>";
+						echo "<th>기수</th>";
+						echo "<th>등급</th>";
+						echo "<th>활동 지역</th>";
+						echo "<th>활동 여부</th>";
+
+					}
+				?>
 				</tr>
 			</thead>
 			<tbody>
@@ -33,12 +54,25 @@
 				if($result = $mysqli->query("SELECT * FROM user order by entry DESC, rank asc, name asc")){
 					while($data = $result->fetch_array(MYSQLI_ASSOC)){
 						echo "<tr>";
-						echo "<td>".$data["name"]."</td>";
-						echo "<td>".$data["major"]."</td>";
-						echo "<td>".(User::getShortStudentID($data["student_id"]))."</td>";
-						echo "<td>".(User::RankInttoStr($data["rank"]))."</td>";
-						echo "<td>".$data["location"]."</td>";
-						echo "<td>".User::EntryInttoStr($data["entry"])."</td>";
+						if($_SESSION["rank"] > 2){
+							echo "<td>".$data["name"]."</td>";
+							echo "<td>".$data["major"]."</td>";
+							echo "<td>".User::getShortStudentID($data["student_id"])."</td>";
+							echo "<td>".User::RankInttoStr($data["rank"])."</td>";
+							echo "<td>".$data["location"]."</td>";
+							echo "<td>".User::EntryInttoStr($data["entry"])."</td>";
+						}else{
+							echo "<td>".$data["name"]."</td>";
+							echo "<td>".User::GenderInttoStr($data["gender"])."</td>";
+							echo "<td>".$data["colleage"]."</td>";
+							echo "<td>".$data["major"]."</td>";
+							echo "<td>".$data["student_id"]."</td>";
+							echo "<td>".User::AppendPhoneHypen($data["phone"])."</td>";
+							echo "<td>".User::AppendClass	($data["class"])."</td>";
+							echo "<td>".User::RankInttoStr($data["rank"])."</td>";
+							echo "<td>".$data["location"]."</td>";
+							echo "<td>".User::EntryInttoStr($data["entry"])."</td>";
+						}
 						echo "</tr>";
 					}
 				}
