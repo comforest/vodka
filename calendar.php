199cfbd4090fe8cbe079b9a58d1402b0c27242b1
HoYean <comforest@naver.com>
2016-12-12 17:35:19 +0900

    Error calendar.php

--- calendar.php
+++ calendar.php
@@ -7,14 +7,23 @@ POST year, month
 header("Content-Type:application/json");
 require_once $_SERVER["DOCUMENT_ROOT"]."/static/php/mysqli.inc";	
 
-$date = $_POST["year"]."-".$_POST["month"]."-1";
-$ldate = $_POST["year"]."-".((int)$_POST["month"]+1)."-1";
+$y = $_POST["year"];
+$m = $_POST["month"];
+$ly = $y;
+$lm = $m + 1;
+if($lm > 12){
+	$lm = 1;
+	++$ly;
+}
+$date = "$y-$m-1";
+$ldate = "$ly-$lm-1";
+
 
 $query = "SELECT calendar_id,date,text,type FROM calendar WHERE date >= '$date' and date < '$ldate' ORDER BY date ASC";
 if ($result = $mysqli->query($query)) {
 	$arr = [];
 	while($data = $result->fetch_array(MYSQLI_NUM)){
- 		$arr[] = array ('id'=>$data[0], 'date' => $data[1], 'text' => $data[2], 'type' => $data[3]);
+		$arr[] = array ('id'=>$data[0], 'date' => $data[1], 'text' => $data[2], 'type' => $data[3]);
 	}
 	echo json_encode($arr);
 
