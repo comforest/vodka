1e35f202f711a45fab8065e033c1926500303bb2
HoYean <comforest@naver.com>
2017-02-01 18:09:24 +0900

    SideMenu

--- static/javascript/header.js
+++ static/javascript/header.js
@@ -1,12 +1,16 @@
 $(document).ready(function(){
 	$(".nav .menu").mouseover(
 		function(){
+			$(".nav .select_menu").parent().parent().hide();
+			$(".nav .select_menu").parent().parent().prev().removeClass("hover");
 			$(this).find(".detail_menu").show();
 			$(this).find(".main_menu").addClass("hover");
 		}
 	).mouseout(function(){
 			$(this).find(".detail_menu").hide();
 			$(this).find(".main_menu").removeClass("hover");
+			$(".nav .select_menu").parent().parent().show();
+			$(".nav .select_menu").parent().parent().prev().addClass("hover");
 		}
 	);
 
